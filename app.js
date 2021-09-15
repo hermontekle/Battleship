34327e7a040aa871d52f55d8096eed5dfa4adcb9
hermontekle <hrmntkl@gmail.com>
2021-09-14 23:56:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let location1 = 3;
+let location1 = 4;
+let location1 = 5;
+
+let guess;
+let hits = 0;
+let guesses = 0;
+
+let isSunk = false;
+
+while (isSunk == false){
+  
+  
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
+  
+}
